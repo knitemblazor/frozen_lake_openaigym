57dd36540f018a86bc4da4322d68e80aaf7ec6a4
NITISH TOM MICHAEL <niteshmichael@gmail.com>
2019-10-30 19:01:11 +0530

    first commit

---
+++ frozenlake.py
@@ -0,0 +1,36 @@
+
+#frozen lake solver 4*4 
+#it is a bruteforce approach with the forbidden states explicitly assigned by me
+ 
+import gym
+import random
+env = gym.make("FrozenLake-v0")
+env.reset()
+env.render()
+reward=0.00
+
+forbidden=[5,7,11,12]
+
+actions = {
+    'Left': 0,
+    'Down': 1,
+    'Right': 2,
+    'Up': 3
+}
+counter=0
+bool=True
+while(bool):
+    counter=counter+1
+    winning_sequence=[random.choice(["Left","Down","Right"]),random.choice(["Left","Down","Right"]),random.choice(["Left","Down"]),random.choice(["Left","Down","Right","Up"])]
+    for a in winning_sequence:
+        new_state, reward, done, info = env.step(actions[a])
+        print()
+        env.render()
+        print("Reward: {:.2f}".format(reward))
+        if new_state in forbidden:
+            env.reset()
+            break
+        if new_state==15:
+            bool=False
+            break
+print(counter)
